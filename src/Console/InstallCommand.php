f5f07a1935bc337e29c1b73bec8408b9c2923668
PedroMontolive <pedro.montolive@gmail.com>
2024-08-27 18:14:07 -0300

    add: alter install comand for config

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -3,6 +3,8 @@
 namespace Pemto\SessionManager\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(name: 'session-manager:install', description: 'Install all of the session-manager resources')]
@@ -29,7 +31,13 @@ class InstallCommand extends Command
      */
     public function handle()
     {
+        $this->components->info('Publishing SessionManager Service Provider...');
 
+        collect([
+            'Configuration' => fn () => $this->callSilent('vendor:publish', ['--tag' => 'session-manager-config']) == 0,
+        ])->each(fn ($task, $description) => $this->components->task($description, $task));
+
+        $this->components->info('SessionManager installed successfully.');
     }
 
 }
